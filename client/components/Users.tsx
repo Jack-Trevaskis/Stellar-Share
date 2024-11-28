718d268e3fca9f65ce5af9a0a8fbab2dcd520f6a
Thomas Craven <thomaswilliamcraven@gmail.com>
2024-11-28 15:00:52 +1300

    start user list component

---
+++ client/components/Users.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query"
+import { getUsers } from "../apis/users"
+import { useNavigate } from "react-router-dom"
+
+function Users() {
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['users'],
+    queryFn: () => getUsers(),
+  })
+
+  const navigate = useNavigate()
+
+  const handleEventClick = (id: number) => {
+    navigate(`/user/${id}`)}
+
+  if (isPending) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    return <p>No users!</p>
+  }
+
+  return (
+    <>
+    <ul>
+      {data.map((user) => (
+        <li key={user.id}>
+          <button onClick={() => handleEventClick(user.id)}></button>
+        </li>
+      ))}
+    </ul>
+    </>
+  )
+}
+
+export default Users
