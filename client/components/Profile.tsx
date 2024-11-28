8434a8136ad1c69088111ef1018cd9e6d0720bc0
DelphyM <dmladek@gmail.com>
2024-11-28 14:54:56 +1300

    Initial Authnetication implementation

---
+++ client/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+export function Profile() {
+  const { user, isAuthenticated, isLoading, isEror } = useAuth0()
+
+  if (isLoading && !user) {
+    return <div>Loading...</div>
+  }
+  if (isEror) {
+    return <div>An error occurred: {isEror.message}</div>
+  }
+  if (!isAuthenticated) {
+    return <div>Please sign in to view your profile.</div>
+  }
+
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>Welcome, {user.name}!</p>
+      <p>Email: {user.email}</p>
+    </div>
+  )
+}
