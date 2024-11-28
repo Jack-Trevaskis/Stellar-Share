192c470959f41bb8db64109a712043f7a61dcf57
Thomas Craven <thomaswilliamcraven@gmail.com>
2024-11-28 14:33:39 +1300

    add db function

--- server/db/users.ts
+++ server/db/users.ts
@@ -4,5 +4,13 @@ import { Users } from '../../models/users.ts'
 // All user DB functions go here
 
 export async function getAllUsers(): Promise<Users[]> {
-  return await db('users').select()
+ const users = await db('users').select(
+  'id',
+  'username',
+  'auth0sub',
+  'phone',
+  'credits',
+  'picture',
+ )
+ return users
 }
