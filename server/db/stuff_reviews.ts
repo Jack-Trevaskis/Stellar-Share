323a172e5f75fdba21dc713c24971fd27cb9a37f
jack-trevaskis <jack.trevaskis1@gmail.com>
2024-11-29 14:21:36 +1300

    initial commit

--- server/db/stuff_reviews.ts
+++ server/db/stuff_reviews.ts
@@ -3,6 +3,14 @@ import { StuffReviews } from '../../models/stuff_reviews.ts'
 
 // All stuff review DB functions go here
 
-// export async function getAllFruits(db = connection): Promise<Fruit[]> {
-//   return db('fruit').select()
-// }
+
+export async function createStuffReview(data: StuffReviews) {
+    try {
+      console.log(data)
+      const [newStuffReviewId] = await connection('daily').insert(data).returning('review_id');
+      return { id: newDailyId, ...data };
+    } catch (error) {
+      console.error("Error creating record:")
+      throw error;
+    }
+  }
