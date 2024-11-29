0e6b4b3ca8d39e7ab3cef6060eff89454e6fca61
gareth-jones117 <garethjonescode@gmail.com>
2024-11-29 18:34:38 +1300

    spelling and correcting stuff (#51)
    * spelling and correcting stuff
    
    * spelling
    
    ---------
    
    Co-authored-by: DelphyM <dmladek@gmail.com>

--- server/db/user_reviews.ts
+++ server/db/user_reviews.ts
@@ -1,33 +1,35 @@
 import connection from './connection.ts'
-import { UserReviews, UserReviewsData} from '../../models/user_reviews.ts'
+import { UserReview, UserReviewData } from '../../models/user_reviews.ts'
 
-
-export async function getAllUserReviews(): Promise<UserReviews> {
-    //not really a useful function, but I'll add it anyway - for science!
-  return await connection('user_reviews')
-  .select('id',
-    'reviewer_auth0_sub' as 'reviewerAuth0Sub', 
+export async function getAllUserReviews(): Promise<UserReview> {
+  //not really a useful function, but I'll add it anyway - for science!
+  return await connection('user_reviews').select(
+    'id',
+    'reviewer_auth0_sub' as 'reviewerAuth0Sub',
     'user_auth0_sub' as 'userAuth0Sub',
     'description',
-    'rating'
+    'rating',
   )
 }
 
 //this is the main function to be used on the front-end - get all the reviews for a specific user
-export async function getAllReviewsOnUser(auth0Sub: string): Promise<UserReviews> {
+export async function getAllReviewsOnUser(
+  auth0Sub: string,
+): Promise<UserReview> {
   return await connection('user_reviews')
-  .where('user_auth0_sub', auth0Sub)
-  .select('id',
-    'reviewer_auth0_sub' as 'reviewerAuth0Sub', 
-    'user_auth0_sub' as 'userAuth0Sub', 
-    'description',
-    'rating'
-  )
+    .where('user_auth0_sub', auth0Sub)
+    .select(
+      'id',
+      'reviewer_auth0_sub' as 'reviewerAuth0Sub',
+      'user_auth0_sub' as 'userAuth0Sub',
+      'description',
+      'rating',
+    )
 }
 
 //ToDo: add a function that gets all reviews that a user has written
 
-export async function addUserReview(review: UserReviewsData) {
+export async function addUserReview(review: UserReviewData) {
   const addReview = await connection('user_reviews').insert({
     description: review.description,
     rating: review.rating,
@@ -38,8 +40,8 @@ export async function addUserReview(review: UserReviewsData) {
 }
 
 export async function deleteUserReview(id: number) {
-    const delUserReview = await connection('user_reviews')
-      .where('id', id)
-      .delete()
-    return delUserReview as number
-  }
+  const delUserReview = await connection('user_reviews')
+    .where('id', id)
+    .delete()
+  return delUserReview as number
+}
