2cf294041e11f2ee66cee7556aa537eea9b03c47
thomas-craven92 <cravenorama@gmail.com>
2024-12-03 10:01:26 +1300

    Image directory (#65)
    * add avatars
    
    * add stuff pics
    
    * merge conflicts
    
    * change pic path
    
    * debug
    
    * change seed data
    
    * change id seed data
    
    * change seed

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
       <h1 className="text-3xl font-bold text-center my-4">Welcome to Stellar Share</h1>
       <p className="text-lg text-center mb-6">The best place in the Milky-Way Galaxy to rent/borrow your astro gear</p>
       <div className="flex flex-col items-center space-y-4"> 
-        <Link to="/stuff" className="text-blue-500 hover:underline">Find astro equipment here</Link>
+        <Link to="/stuff" className="bg-white hover:underline">Find astro equipment here</Link>
         <Link to="/users" className="text-blue-500 hover:underline">See all our good (and not-so-good) space-cadets here</Link>
         <IfAuthenticated>
           <Link to="/addStuff" className="text-blue-500 hover:underline">Add your own items here</Link>
